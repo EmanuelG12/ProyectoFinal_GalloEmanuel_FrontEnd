4146d5cb7922ffed3e0245f7f9e8bbac927f8ac5
Emanuel Gallo <Emanuelgallo02@hotmail.com>
2022-07-20 21:43:59 -0300

    Ultimos retoques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { EducacionYexperienciaComponent } from './components/educacion-yexperiencia/educacion-yexperiencia.component';
+import { HysComponent } from './components/hys/hys.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    PresentacionComponent,
+    SobreMiComponent,
+    EducacionYexperienciaComponent,
+    HysComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/educacion-yexperiencia/educacion-yexperiencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-educacion-yexperiencia',
+  templateUrl: './educacion-yexperiencia.component.html',
+  styleUrls: ['./educacion-yexperiencia.component.css']
+})
+export class EducacionYexperienciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
